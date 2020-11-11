0fba098fd61e76614ed2a3174be7a3d0ac34220f
ezzee10 <ezequiel.colombano@gmail.com>
2020-11-11 18:06:08 -0300

    se agrega excepcion y un testeo de ella para verificar que no se puedan anotar mas puntos una vez que existe un ganador

--- PartidoTenis/src/test/java/ar/edu/untref/dyasc/TestPuntaje.java
+++ PartidoTenis/src/test/java/ar/edu/untref/dyasc/TestPuntaje.java
@@ -554,30 +554,28 @@ public class TestPuntaje {
 			
 	}
 	
-	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void seIntentaAgregarMasPuntosLuegoDePartidoFinalizadoYseAdvierte() {
 		
-		for(int i=0; i<24; i++) {
-			partido.anotarPunto(NumeroDeJugador.DOS);  //1SET
-		}
-	
-	
-		for(int i=0; i<24; i++) {
-			partido.anotarPunto(NumeroDeJugador.DOS); //2 SETS
-		}
-		
-		for(int i=0; i<24; i++) {
-			partido.anotarPunto(NumeroDeJugador.DOS); //3 SETS
-		}
-		
-		partido.anotarPunto(NumeroDeJugador.DOS); //ANOTAR PUNTO UNA VEZ QUE HAY UN GANADOR
-	
-		
-		Assert.assertEquals(0, partido.obtenerSets(NumeroDeJugador.UNO));
-		Assert.assertEquals(3, partido.obtenerSets(NumeroDeJugador.DOS));
-		Assert.assertEquals(NumeroDeJugador.DOS, partido.obtenerGanador());
-
+		 try {
+			 for(int i=0; i<24; i++) {
+					partido.anotarPunto(NumeroDeJugador.DOS);  //1SET
+				}
+			
+			
+				for(int i=0; i<24; i++) {
+					partido.anotarPunto(NumeroDeJugador.DOS); //2 SETS
+				}
+				
+				for(int i=0; i<24; i++) {
+					partido.anotarPunto(NumeroDeJugador.DOS); //3 SETS
+				}
+				
+				partido.anotarPunto(NumeroDeJugador.DOS); //ANOTAR PUNTO UNA VEZ QUE HAY UN GANADOR
+			
+		    } catch (IllegalArgumentException ex) {
+		        Assert.assertEquals("Ya hay un ganador no se puede anotar más puntos", ex.getMessage());
+		    }
 			
 	}
 		
